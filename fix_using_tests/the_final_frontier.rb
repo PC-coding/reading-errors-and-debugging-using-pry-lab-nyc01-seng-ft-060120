4a491dfbee5fb9dd7dfe3e6126c6ea944593d1d4
Partap Chauhan <partapchauhan8@gmail.com>
2020-05-20 19:21:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
 require 'pry'
 
-def generate_star_date
+def star_date
   binding.pry
   (rand(100000) + 400000) / 10.0
 end
@@ -11,14 +11,9 @@ def state_log(star_date)
   binding.pry
   "Captain's Log, star date #{star_date}."
 end
-
-def crew
-  [Partap, Jason, Sarina]
-end
   
 def engage
   binding.pry
   puts state_log(star_date)
   date = generate_star_date
-  greet_crew(crew)
 end
